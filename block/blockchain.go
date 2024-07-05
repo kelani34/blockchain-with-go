11d9db292005e68e1133e31e8c177749e4365aea
kelani34 <kelanitaiwo34@gmail.com>
2024-07-05 02:35:42 +0100

    update project refactor for wallet creation

--- block/blockchain.go
+++ block/blockchain.go
@@ -1,4 +1,4 @@
-package main
+package block
 
 import (
 	"crypto/sha256"
@@ -180,32 +180,4 @@ func (bc *BlockChain) Print() {
 		fmt.Printf("%s \n", strings.Repeat("*", 25))
 		block.Print()
 	}
-}
-
-func init() {
-	log.SetPrefix("Blockchain: ")
-}
-
-func main() {
-	//block := &Block{nonce: 1}
-	//
-	//fmt.Println(block.Hash())
-
-	myBlockChainAddress := "my_block_chain_address"
-
-	blockChain := NewBlockChain(myBlockChainAddress)
-	blockChain.Print()
-
-	blockChain.AddTransaction("A", "B", 1.0)
-	blockChain.Mining()
-	blockChain.Print()
-
-	blockChain.AddTransaction("C", "D", 899.0)
-	blockChain.AddTransaction("X", "Y", 9999.0)
-	blockChain.Mining()
-	blockChain.Print()
-
-	fmt.Printf("my_block_chain_address %.2f\n", blockChain.CalculateTotalAmount(myBlockChainAddress))
-	fmt.Printf("C %.2f\n", blockChain.CalculateTotalAmount("C"))
-	fmt.Printf("D %.2f\n", blockChain.CalculateTotalAmount("D"))
 }
